c84f984fce7650cf986f7595a485542af6fcc7ca
Eduardo Lopez <eduardo.contreras@wizeline.com>
2019-10-03 15:23:14 -0500

    chore: add test for login and constans required

---
+++ functionalTests/test/loginTests.js
@@ -0,0 +1,88 @@
+import { USER_CREDENTIALS, USER_PASSWORD, VALID_FB_TOKEN, ALERT_SUCCESS_TEXT, ALERT_ERROR_TEXT, INVALID_USER_CREDENTIALS, INVALID_USER_PASSWORD } from '../utils/constants'
+import { assert } from 'chai'
+
+import loginScreen from '../screens/LoginScreen'
+import resetPasswordScreen from '../screens/ResetPasswordScreen'
+import splashScreen from '../screens/SplashScreen'
+import updateScreen from '../screens/UpdateScreen'
+
+beforeEach(function () {
+    browser.reset()
+})
+
+describe('Login functionality for sample app: ', () => {
+    it('Anonymously login', () => {
+        splashScreen.clickAnonymousLoginCTA()
+
+        const updateScreenLoaded = updateScreen.isUpdateScreenLoaded()
+
+        assert(updateScreenLoaded, true)
+    })
+
+    it('Login valid user and password', () => {
+        splashScreen.clickSignInCTA()
+        loginScreen.enterLoginCredentials(USER_CREDENTIALS, USER_PASSWORD)
+
+        const alertTitle = loginScreen.getAlertTitle()
+        const alertMessage = loginScreen.getAlertMessage()
+
+        assert.equal(alertTitle, ALERT_SUCCESS_TEXT)
+        assert.isNotEmpty(alertMessage)
+    })
+
+    it('Login invalid user', () => {
+        splashScreen.clickSignInCTA()
+        loginScreen.enterLoginCredentials(INVALID_USER_CREDENTIALS, USER_PASSWORD)
+
+        const alertTitle = loginScreen.getAlertTitle()
+        const alertMessage = loginScreen.getAlertMessage()
+
+        assert.equal(alertTitle, ALERT_ERROR_TEXT)
+        assert.isNotEmpty(alertMessage)
+    })
+
+    it('Login invalid password', () => {
+        splashScreen.clickSignInCTA()
+        loginScreen.enterLoginCredentials(USER_CREDENTIALS, INVALID_USER_PASSWORD)
+
+        const alertTitle = loginScreen.getAlertTitle()
+        const alertMessage = loginScreen.getAlertMessage()
+
+        assert.equal(alertTitle, ALERT_ERROR_TEXT)
+        assert.isNotEmpty(alertMessage)
+    })
+
+    it.skip('Facebook token login', () => {
+        splashScreen.clickSignInCTA()
+        loginScreen.enterFacebookToken('VALID_FB_TOKEN')
+    })
+
+    it('Facebook incorrect token login', () => {
+        splashScreen.clickSignInCTA()
+        loginScreen.enterFacebookToken(VALID_FB_TOKEN)
+
+        const alertTitle = loginScreen.getAlertTitle()
+        const alertMessage = loginScreen.getAlertMessage()
+
+        assert.equal(alertTitle, ALERT_ERROR_TEXT)
+        assert.isNotEmpty(alertMessage)
+    })
+
+    it('Show password', () => {
+        splashScreen.clickSignInCTA()
+        loginScreen.showPassword(USER_PASSWORD)
+
+        const showPassword = loginScreen.getPasswordText()
+
+        assert.equal(showPassword, USER_PASSWORD)
+    })
+
+    it('Forgot password', () => {
+        splashScreen.clickSignInCTA()
+        loginScreen.clickForgotPassword()
+
+        const resetScreenLoaded = resetPasswordScreen.isResetPasswordScreenLoaded()
+
+        assert(resetScreenLoaded, true)
+    })
+})
